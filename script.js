fd67246304575799fbdd033d9f69f26351e26123
Aleksander Jaworski <aleck156@gmail.com>
2022-03-16 07:12:55 +0100

    complete theory part, moving to the challenge #1

--- script.js
+++ script.js
@@ -43,6 +43,16 @@ const restaurant = {
       `You've ordered (${this.starterMenu[sI]}, ${this.mainMenu[mI]}) to be delivered to ${a} at ${t}.`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
 };
 
 //------------------------------------------------------
@@ -143,3 +153,202 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 1,
 });
+
+//------------------------------------------------------
+// SPREAD OPERATOR
+// ...
+// takes all of the elements
+// doesn't create new variables
+// iterables - arrays, strings, maps, sets, BUT NOT OBJECTS
+
+const arr = [7, 8, 9];
+const badNewArray = [1, 2, ...arr];
+console.log(...badNewArray);
+
+const newMenu = [...restaurant.mainMenu, 'my New creation, buahahahaha!'];
+console.log(newMenu);
+
+// USAGE
+// shallow copies of arrays
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// merging 2 arrays together
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2];
+console.log(...arr3);
+const wholeMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(wholeMenu);
+
+// spreading strings ...
+const str = 'Aleksander';
+const strArray = [...str, ' ', 'J.'];
+console.log(strArray);
+// string literals are not a place to use spread operator, as it's not a place that expects multiple values separated by a comma
+// console.log(`${...str} won't work properly`);
+
+/*
+const ingredients = [
+  prompt(`Let's make pasta, ingredient1: `),
+  prompt(`Let's make pasta, ingredient2: `),
+  prompt(`Let's make pasta, ingredient3: `),
+];
+
+restaurant.orderPasta(...ingredients);
+*/
+
+// objects are not iterable ... yet spread operator works on them too!
+const newRestaurant = {
+  ...restaurant,
+  founder: 'Giuseppe Orlando',
+  foundedIn: 1991,
+};
+
+console.log(newRestaurant);
+
+// we did make a copy of original object!
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurant.name);
+
+//------------------------------------------------------
+// REST OPERATOR
+// on the left side of equal sign
+// it has to be the last operator in the line
+
+// 1) destructuring
+const [aa, bb, ...rest] = [1, 2, 3, 4, 5, 6];
+console.log(aa, bb, rest);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// in objects ...
+// the remaining elements will be collected into a new object, not an array
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) functions
+const add = function (...numbers) {
+  // let sum = 0;
+  // for (let i = 0; i < arguments.length; i++) {
+  //   sum += arguments[i];
+  // }
+  // return sum;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
+
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
+console.log(add(...testArray));
+
+const x = [23, 5, 7].reduce((a, b) => a + b, 0);
+console.log(x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+
+restaurant.orderPizza('mushrooms');
+
+//------------------------------------------------------
+// LOGICAL OPERATORS
+// can use any data type
+// can return ANY data type
+// short-circuiting evaluation
+// the first non-falsy value gets returned on the spot, or the last falsy
+console.log('------ OR -------');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas'); // 'Jonas'
+console.log(true || 0); // true
+console.log(undefined || null); // null
+
+// &&
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); // 'Hello'
+
+restaurant.numGuests = 23;
+console.log(typeof restaurant.numGuests);
+const guests1 = restaurant.numGuests != undefined ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('------ AND -------');
+// immediately returns the first falsy value or return the last truly
+console.log(0 && 'jonas');
+console.log(true && 'jonas' && 17);
+console.log('Hello' && 23 && null && 'Jonas'); // null
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+// this can be replaced with
+// no need for if-statement
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+//------------------------------------------------------
+// NULLISH COALESCING OPERATOR
+// ES2020
+// nullish values instead of falsy
+// null, undefined
+// doesn't include 0 or an empty string - they're treated like truthy
+restaurant.numGuests = 0;
+console.log(typeof restaurant.numGuests);
+const guests3 = restaurant.numGuests ?? 10;
+console.log(guests3);
+
+const guests4 = restaurant.numGuests || 10;
+console.log(guests4);
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+//------------------------------------------------------
+// LOGICAL ASSIGNMENT OPERATOR
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// conventional way
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// logical assignment operator
+// it operates the exactly same way as the above
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// console.log(rest1, rest2);
+
+// the bug happens when the value we test against is 0
+// it's a falsy value, thus the second argument gets assigned
+// which leads to errors, because sometimes 0 and '' are valid values
+// in that case, it's better to use ??=
+// LOGICAL NULLISH ASSIGNMENT (null or undefined only)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// LOGICAL && OPERATOR
+
+// rest1.owner = rest1.owner && 'anonymous';
+rest1.owner &&= 'anonymous';
+rest2.owner &&= 'anonymous';
+
+// the difference between
+//    rest1.owner &&= 'anonymous';
+// and
+//    rest1.owner = rest1.owner && 'anonymous';
+// is that in the first case owner does not exists, whereas in the second case owner gets created, yet is set with **undefined** value
+
+console.log(rest1, rest2);
