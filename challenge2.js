e506c7400aab42434e1ee323d32607dd0b6e7be8
Aleksander Jaworski <aleck156@gmail.com>
2022-03-16 11:27:59 +0100

    complete challenge #2

---
+++ challenge2.js
@@ -0,0 +1,71 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// 2.
+let avg = 0;
+for (const odd of Object.values(game.odds)) {
+  avg += odd;
+}
+console.log(`The average odd is ${avg / Object.keys(game.odds).length}`);
+
+// 3.
+// console.log(`Odd of victory ${game.team1]}: ${team1}`);
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamName = game[team] ? `victory ${game[team]}` : 'draw';
+  console.log(`Odd of ${teamName}: ${odd}`);
+}
+
+// 4.
+const scorers = {};
+
+for (const player of game.scored) {
+  console.log(player);
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+  // scorers[player] ??= 0;
+  // scorers[player]++;
+}
+
+console.log(scorers);
