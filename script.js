1500676c09fe7b4d3ebd4853de72cd933af7e54b
Aleksander Jaworski <aleck156@gmail.com>
2022-03-16 10:34:32 +0100

    learning ES6 and ES2020

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const weekdays2 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays2[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays2[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays2[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -12,28 +29,17 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+
+  // ES6 ENHANCED OBJECT LITERALS
+  openingHours,
 
   // receiving just one object, then destructuring it
   // names have to match the object passed into
   // order of parameters does NOT matter
-  orderDelivery: function ({
+  orderDelivery({
     starterIndex: sI = 1,
     mainIndex: mI = 0,
     time: t = '22:00',
@@ -44,13 +50,14 @@ const restaurant = {
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  // ES6 SYNTAX IMPROVEMENT
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient, otherIngredients);
   },
 };
@@ -94,8 +101,8 @@ console.log(p, q, r);
 // OBJECT DESTRUCTURING
 // using curly braces, as we want to create an object too
 // using the exact same property names as in the source object
-const { name: localName, openingHours, categories } = restaurant;
-console.log(localName, openingHours, categories);
+const { name: localName, openingHours2, categories } = restaurant;
+console.log(localName, openingHours2, categories);
 
 // changing variable names
 const [newName, newOpeningHours, newCategories] = [
@@ -352,3 +359,88 @@ rest2.owner &&= 'anonymous';
 // is that in the first case owner does not exists, whereas in the second case owner gets created, yet is set with **undefined** value
 
 console.log(rest1, rest2);
+
+//------------------------------------------------------
+// FOR ... OF ... LOOP
+// you can use continue and break keywords here
+// no access to index of elements
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) {
+  console.log(item);
+}
+
+// how to get the index, in case you may need it
+// array iterators
+// console.log([...menu.entries()]);
+for (const [index, item] of menu.entries()) {
+  console.log(`${index + 1}. ${item}`);
+}
+
+//------------------------------------------------------
+// ENHANCED OBJECT LITERALS
+// 3 ways in ES6
+// 1.
+// 2.
+// 3. we can now computer variable name, instead of typing it statically
+
+//------------------------------------------------------
+// OPTIONAL CHAINING
+// ?.
+// avoid raising an error
+// get an undefined value when an optional property does not exist
+// works on null and undefined
+// 0 and empty strings are still valid
+console.log(restaurant.openingHours?.sat?.open);
+
+// example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const el of days) {
+  const open = restaurant.openingHours?.[el] ?? 'closed';
+  restaurant.openingHours?.[el] && console.log(`Restaurant is open on ${el}`);
+  // console.log(
+  //   `Restaurant is ${restaurant.openingHours?.[el] && 'not '} open on ${el}`
+  // );
+  // console.log(el);
+}
+
+// the same is useful for methods
+console.log(restaurant.order?.(2, 1) ?? `Method does not exist`);
+
+console.log(restaurant.orderRisotto?.(2, 1) ?? `Method does not exist`);
+
+// but what about arrays ...?
+const users = [
+  {
+    name: 'Al-Iskandir',
+    email: 'gmail@r41lgun.mock',
+  },
+];
+
+console.log(users[7]?.name ?? 'Such a player does not exist');
+
+//------------------------------------------------------
+// LOOPING OVER OBJECT PROPERTIES - names and values - WHICH ARE NOT ITERABLE
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  // console.log(day);
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// iterating over values
+const values = Object.values(openingHours);
+console.log(values);
+
+// .entries()
+const allEntries = Object.entries(openingHours);
+// console.log(allEntries);
+
+for (const [day, { open: o, close: c }] of allEntries) {
+  console.log(`On ${day}, we open at ${o} and close at ${c}`);
+}
